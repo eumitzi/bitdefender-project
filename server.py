2abd430325740be934e74269ab4d57620d1745ee
eumitzi <39062930+eumitzi@users.noreply.github.com>
2019-03-31 23:14:00 +0300

    refactor the server - no change of functionality

--- server.py
+++ server.py
@@ -1,21 +1,25 @@
-from flask import Flask, request
-from flask_pymongo import PyMongo
+from flask import Flask, jsonify, request
+from flask_restful import Api, Resource
+from pymongo import MongoClient
 
 app = Flask(__name__)
-app.config["MONGO_URI"] = "mongodb://localhost:27017/mongo-practice"
-mongo = PyMongo(app)
+api = Api(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+client = MongoClient("mongodb://localhost:27017")
+#connect to the db
+db = client.test
+#get the files collection 
+files = db["files"]
 
-@app.route("/scan", methods=["GET"])
-def queryDb():
+class Scan(Resource):
+    def get(self):
+        fromClient = request.args["md5"]
+        firstFile = files.find_one({"md5": fromClient})
+        print(firstFile)
+        return "scanned"
 
-    fromClient = request.args["md5"]
+api.add_resource(Scan, '/scan')
 
-    data = mongo.db.files.find_one({"md5": fromClient})
-    
-    print(data)
-    print(request)
-    return "scanned"
+if __name__=="__main__":
+    app.run(host='127.0.0.0')
+
