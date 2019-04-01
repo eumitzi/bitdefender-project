10ae6decd948ade5dbfc52cfb57af9c6b511ba07
eumitzi <39062930+eumitzi@users.noreply.github.com>
2019-04-01 16:20:19 +0300

    added resource for download

--- server.py
+++ server.py
@@ -11,14 +11,26 @@ db = client.test
 #get the files collection 
 files = db["files"]
 
+#create logging function
+
+#Upload the file
 class Scan(Resource):
+
+    #call logging file  
+
     def get(self):
         fromClient = request.args["md5"]
         firstFile = files.find_one({"md5": fromClient})
         print(firstFile)
         return "scanned"
 
+#Download the file
+class Download(Resource):
+    def get(self):
+        return "downloaded"
+
 api.add_resource(Scan, '/scan')
+api.add_resource(Download, '/download')
 
 if __name__=="__main__":
     app.run(host='127.0.0.0')
