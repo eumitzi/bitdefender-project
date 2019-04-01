d8625bd5368e55356355ed1fe94f97acc2f8d57b
eumitzi <39062930+eumitzi@users.noreply.github.com>
2019-04-01 21:17:57 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,32 +1,43 @@
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
 from pymongo import MongoClient
+from datetime import datetime
+import gridfs
 
 app = Flask(__name__)
 api = Api(app)
 
 client = MongoClient("mongodb://localhost:27017")
 #connect to the db
-db = client.test
+db = client.filesDB
 #get the files collection 
-files = db["files"]
+files = db["fs.files"]
 
-#create logging function
+fs = gridfs.GridFS(db)
+
+#logging function
+def logToDB(scannedMd5, status):
+    currentTime = datetime.now()
+    db.log.insert_one({"timestamp": currentTime, "scanned": scannedMd5, "result": status})
+
 
 #Upload the file
 class Scan(Resource):
-
-    #call logging file  
-
     def get(self):
-        fromClient = request.args["md5"]
-        firstFile = files.find_one({"md5": fromClient})
-        print(firstFile)
-        return "scanned"
+       clientStatus = request.args["status"]
+       clientMd5    = request.args["md5"]
+       if clientStatus == "more_data": 
+           fs.put(b"data2", md5=clientMd5, status=clientStatus)
+           logToDB(clientMd5, clientStatus)       
+       return "scanned"
 
 #Download the file
 class Download(Resource):
     def get(self):
+        clientMd5 = request.args["md5"]
+        for grid_data in fs.find({"md5": clientMd5}):
+            data = grid_data.read()
+      # print(data)
         return "downloaded"
 
 api.add_resource(Scan, '/scan')
