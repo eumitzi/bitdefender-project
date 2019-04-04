dd5af7fec9377a069c71754ee3f0a90c95397fdb
eumitzi <39062930+eumitzi@users.noreply.github.com>
2019-04-04 04:49:57 +0300

    Update server.py

--- server.py
+++ server.py
@@ -11,38 +11,34 @@ client = MongoClient("mongodb://localhost:27017")
 #connect to the db
 db = client.filesDB
 #get the files collection 
-files = db["fs.files"]
+files = db["files"]
 
 fs = gridfs.GridFS(db)
 
-#logging function
+#Logging function
 def logToDB(scannedMd5, status):
     currentTime = datetime.now()
     db.log.insert_one({"timestamp": currentTime, "scanned": scannedMd5, "result": status})
 
-
 #Upload the file
 class Scan(Resource):
-    def get(self):
-       clientStatus = request.args["status"]
-       clientMd5    = request.args["md5"]
-       if clientStatus == "more_data": 
-           fs.put(b"data2", md5=clientMd5, status=clientStatus)
-           logToDB(clientMd5, clientStatus)       
-       return "scanned"
+    def post(self):
 
-#Download the file
-class Download(Resource):
-    def get(self):
-        clientMd5 = request.args["md5"]
-        for grid_data in fs.find({"md5": clientMd5}):
-            data = grid_data.read()
-      # print(data)
-        return "downloaded"
+        #Get posted data
+        postedData = request.get_json()
+        clientMd5 = postedData['md5']
+        clientStatus = postedData['status']
 
-api.add_resource(Scan, '/scan')
-api.add_resource(Download, '/download')
-
+        if clientStatus == "more_data": 
+            retMap = {
+                'message': 'Need more data about this file',
+                'md5': clientMd5,
+			    'status': clientStatus
+		    	}
+            fs.put(b"data2", md5=clientMd5, status=clientStatus)
+            logToDB(clientMd5, clientStatus)
+            return jsonify(retMap)	
+		   
+api.add_resource(Scan, "/scan")
 if __name__=="__main__":
     app.run(host='127.0.0.0')
-
