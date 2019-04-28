47cb4ebf8aa938d9de12136989922ff665b3ec91
Snnn26 <paveliosif00@gmail.com>
2019-04-28 20:20:07 +0300

    finished client but not tested

---
+++ client/client_interface.py
@@ -0,0 +1,64 @@
+import argparse
+import client_program
+from tqdm import tqdm
+from termcolor import colored
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument("files", nargs='+',
+                    help="display a square of a given number")
+parser.add_argument("-s", "--scanHash", help="scans the database for the file \
+                    hashes", action="store_true")
+parser.add_argument("-b", "--scanBuff", help="takes vital info from the files and \
+                    checks it", action="store_true")
+parser.add_argument("-c", "--clean", help="disinfects the files", action="store_true")
+
+
+args = parser.parse_args()
+
+
+#{items: [{file: '', status: ''}...]}
+
+def print_one_two_three(answer):
+    items = answer['items']
+    for ans in items:
+        if ans['status'] == 'clean':
+            print(colored(ans['file'], 'blue'), 'is', colored(ans['status'], 'green'))
+        elif ans['status'] == 'malware':
+            print(colored(ans['file'], 'blue'), 'is', colored(ans['status'], 'red'))
+        elif ans['status'] == 'failed':
+            print(colored(ans['file'], 'blue'), colored(ans['status'], 'red'))
+        elif ans['status'] == 'succeded':
+            print(colored(ans['file'], 'blue'), colored(ans['status'], 'yellow'))
+        else:
+            print(colored(ans['file'], 'blue'), 'is', colored(ans['status'], 'grey'))
+
+
+if (not args.scanHash) and (not args.scanBuff) and (not args.clean):
+    parser.error('No options provided. Please specify exaclty one.')
+
+
+if (args.scanHash + args.scanBuff + args.clean) >= 2:
+    parser.error('Too many options provided. Please specify exactly one.')
+
+
+if args.scanHash:
+    answer = client_program.stage_one(args.files)
+    print_one_two_three(answer)
+
+
+if args.scanBuff:
+    answer = client_program.stage_two(args.files)
+    print_one_two_three(answer)
+
+
+if args.clean:
+    no_of_files = len(args.files)
+    files_done = 0
+    answer = {}
+    answer['items'] = []
+    for file_number in tqdm(range(no_of_files)):
+        status = client_program.stage_three(args.files[file_number])
+        answer['items'].append({'file': args.files[file_number], 'status': status})
+    print("Finished.")
+    print_one_two_three(answer)
---
+++ client/client_program.py
@@ -0,0 +1,126 @@
+import requests
+import hashlib
+import pefile
+import json
+
+with open('config.json') as config_file:
+    data = json.load(config_file)
+
+ID_CLIENT = data['DEFAULT']['ID_CLIENT']
+SERVER_LINK = data['DEFAULT']['SERVER_LINK']
+
+answer = {}
+answer['items'] = []
+
+# {items: [{file:, status:}...]}
+
+
+def stage_one(files):
+    items = {}
+    items['id_client'] = ID_CLIENT
+    files_md5 = []
+    
+    files_to_md5 = {}
+
+    for i in files:
+        with open(i, "rb") as file:
+            files_md5.append(hashlib.md5(file.read()).hexdigest())
+            files_to_md5[files_md5[-1]] = i
+
+    items['hash'] = files_md5
+
+    for ans in requests.post(SERVER_LINK + 'scanHash', json=items).json()['result']:
+        answer['items'].append({'file': files_to_md5[ans['md5']],
+                                'status': ans['status']})
+
+    return answer
+
+
+def wait_for_buffer_answer(job_id):
+    
+    response = requests.get(SERVER_LINK + 'scanBuffer/' + job_id).json();
+    
+    while response['status'] != 'ready':
+        time.sleep(2)
+        response = requests.get(SERVER_LINK + 'scanBuffer/' + job_id).json();
+
+    return response
+
+
+def stage_two(files):
+    #check job until ready
+    #return answer
+
+    items = {}
+    items['id_client'] = ID_CLIENT
+    items['buffers'] = []
+
+    files_to_md5 = {}
+
+    for file in files:
+        item = {}
+
+        with open(file, "rb") as file:
+            item["md5"] = hashlib.md5(file.read()).hexdigest()
+            files_to_md5[item['md5']] = file
+
+        pe = pefile.PE(file)
+        sec = []
+
+        for i in pe.sections:
+            section = {"n": i.Name[:i.Name.index(b'\0')].decode("utf-8"), 'sa': hex(i.VirtualAddress),
+                       's': hex(i.SizeOfRawData), 'c': hex(i.Characteristics)}
+            sec.append(section)
+
+        item["fh"] = {"ns": hex(pe.FILE_HEADER.NumberOfSections),
+                      "c": hex(pe.FILE_HEADER.Characteristics)}
+        item["oh"] = {"m": hex(pe.OPTIONAL_HEADER.Magic),
+                      "ep": hex(pe.OPTIONAL_HEADER.AddressOfEntryPoint)}
+        item["sec"] = sec
+
+        items["buffers"].append(item)
+
+    job_id = requests.post(SERVER_LINK + 'scanBuffer', json=items).json()['id']
+    
+    job_json = wait_for_buffer_answer(job_id)
+
+    for ans in job_json['result']:
+        answer['items'].append({'file': files_to_md5[ans['md5']],
+                                'status': ans['status']})
+
+    return answer
+
+
+def wait_for_buffer_answer(job_id):
+    
+    response = requests.get(SERVER_LINK + 'uploadFile/' + job_id).json();
+    
+    while response['status'] != 'ready':
+        time.sleep(2)
+        response = requests.get(SERVER_LINK + 'uploadFile/' + job_id).json();
+
+    return response
+
+
+def stage_three(file):
+    
+    with open(file, "rb") as hfile:
+        old_md5 = hashlib.md5(hfile.read()).hexdigest()
+
+    upload_files = {'file': open(file, 'rb')}
+    r = requests.post(SERVER_LINK + 'uploadFile/' + old_md5, files=upload_files)    
+
+    job_json = wait_for_upload_answer(r.json()['jobID'])
+    
+    if job_json['result']['status'] == 'clean':
+        new_md5 = job_json['result']['md5_clean']
+        os.remove(file)
+
+        with requests.get(SERVER_LINK + 'downloadFile/' + new_md5, stream=True) as r:
+            r.raise_for_status()
+            with open(file, 'wb') as f: #nu stiu daca mai merge ???
+                for chunk in r.iter_content(chunk_size=8192):
+                    if chunk:
+                        f.write(chunk)
+
+    return job_json['result']['status']
