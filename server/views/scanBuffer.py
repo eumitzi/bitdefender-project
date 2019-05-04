eb806032adc7c4f5835dc3ce31547d591346ec47
Florea Marius <47918060+mariusflorea98@users.noreply.github.com>
2019-05-04 22:06:10 +0300

    stage 1 & 2, not tested with iosif's client yet

--- server/views/scanBuffer.py
+++ server/views/scanBuffer.py
@@ -1,7 +1,59 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from . import DB
+import json
+from ..workers import scan
 
 scan_buffer = Blueprint('scan_buffer', __name__)
 
+base_Coll = DB.getBaseColl()
+
+from rq.job import Job
+from redis import Redis
+from rq import Queue
+redis_conn = Redis()
+q = Queue(connection=redis_conn)
+
+
+
 @scan_buffer.route('/scanBuffer',methods=["POST", "GET"])
 def scanBuffer():
-    return "dummy from scanBuffer"
+
+    if request.method == 'POST':
+        data = request.get_json()
+        #id_client = data['id_client']  # use id_client for logging
+        job = q.enqueue(scan.detect,data)
+        return job.id
+
+
+    if request.method == 'GET':
+        req = request.args.get('id_job')
+        #id = req.split('=',0)
+        id=req
+
+
+        job2 = Job.fetch(id, connection=redis_conn)
+        
+        if job2.get_status() == 'started':
+            return 'pending'
+        elif job2.get_status() == 'finished':
+            print(job2.result)
+
+            # for file in job2.result:
+            #   base_Coll.insert_one({'md5': file['md5'], 'status': file['status']})
+
+            response = {'status': 'ready',
+                        'result': []
+                        }
+
+            response['result'].append(job2.result)
+            return json.dumps(response)
+
+
+        elif job2.get_status() == 'failed':
+            return 'failed'
+
+    return "scanBuffer default"
+
+
+
+
--- server/views/scanHash.py
+++ server/views/scanHash.py
@@ -1,9 +1,32 @@
 from flask import Blueprint
-
+from flask import request
+from . import DB
+import json
 scan_hash = Blueprint('scan_hash', __name__)
-
 
 
 @scan_hash.route('/scanHash',methods=["POST", "GET"])
 def scanHash():
-    return "dummy"
+    statusList = {}
+    response = []
+
+    data = request.get_json()
+    hashes = data['md5']
+
+    #id_client = data['id_client']
+
+    base_Coll = DB.getBaseColl()
+
+    for md5 in hashes:
+        hash = base_Coll.find_one({"md5": md5})
+        if hash:
+            statusList[md5] = hash['status']
+        else:
+            statusList[md5] = 'more_data'
+
+    for md5, status in statusList.items():
+        response.append({'md5':md5,'status':status})
+
+    response=json.dumps(response)
+    return response
+
