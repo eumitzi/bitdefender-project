b036b442365a2be675495451d5a5dd6435f4c555
eumitzi <39062930+eumitzi@users.noreply.github.com>
2019-05-23 08:58:18 +0300

    added blueprint for download

--- server/app.py
+++ server/app.py
@@ -2,22 +2,10 @@ from flask import Flask
 from views.scanHash import scan_hash
 from views.scanBuffer import scan_buffer
 from views.uploadFile import upload_file
+from views.downloadFile import download_file
 
 app = Flask(__name__)
 app.register_blueprint(scan_hash)
 app.register_blueprint(scan_buffer)
 app.register_blueprint(upload_file)
-
-# from flask import Flask
-# from pymongo import MongoClient
-
-# def main():
-#     app=FLask(__name__)
-#     mongoClient= MongoClient("mongodb://localhost:27017")
-#     db = mongoClient.bitDef
-#     base_Coll=db["files"]
-#     app.run(debug=True)
-
-
-# if __name__=='__main__':
-#     main()
+app.register_blueprint(download_file)
